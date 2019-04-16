89bb548a0ecaa0629fafc31d44ae4e780f0469aa
Paul Tobin <tobster111@gmail.com>
2019-04-16 10:35:31 +0100

    Added actions and boiler plate for redex in PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions';
 
 class PostList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
-    return (
-      <div>
-        Post List
-      </div>
-    );
+    return <div>Post List</div>;
   }
 }
 
-export default PostList;
+const mapStateToProps = (state, ownProps) => ({
+
+
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostList);
