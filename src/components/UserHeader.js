c0c12de11eeaf8d81fc1de35de90d228fb499299
Paul Tobin <tobster111@gmail.com>
2019-04-18 12:21:05 +0100

    Refactored the UserHeader component, using componentDidMount and brought in redux

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
+import { fetchUser } from '../actions';
 class UserHeader extends Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div>
-        <h4>User Header</h4>
+        <h4>{this.props.name}</h4>
       </div>
     );
   }
 }
 
-export default UserHeader;
+const mapStateToProps = state => {
+  return { user: state.user };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
