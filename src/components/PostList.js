669bc7333f7740bfe032be07f1edf7164fccdc31
Paul Tobin <tobster111@gmail.com>
2019-04-18 11:30:12 +0100

    Added renderList method to PostList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,9 +8,26 @@ class PostList extends Component {
     this.props.fetchPosts();
   }
 
+  renderList() {
+    return this.props.posts.map(({ id, title, body }) => {
+      return (
+        <div className="item" key={id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{title}</h2>
+              <p>{body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  //<li style={{ listStyle: 'none' }}>{post.title}</li>
   render() {
-    console.log(this.props.posts)
-    return <div>Post List</div>;
+    console.log(this.props.posts);
+    return <div>{this.renderList()}</div>;
   }
 }
 
